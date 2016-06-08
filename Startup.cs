7565032c0255554c800aeea52614ebe7c220d1ad
Matt Gross <mattkgross@gmail.com>
2016-06-07 22:12:29 -0600

    Base Project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkoutLogPro.Startup))]
+namespace WorkoutLogPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
