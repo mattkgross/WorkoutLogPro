37284d0b2920fb290990b6ef7c92a75688f1d9a7
Matt Gross <mattkgross@gmail.com>
2016-06-07 22:29:45 -0600

    NuGet + Project Config

---
+++ Areas/Teams/TeamsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WorkoutLogPro.Areas.Teams
+{
+    public class TeamsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Teams";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Teams_default",
+                "Teams/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
