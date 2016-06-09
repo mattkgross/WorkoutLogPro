b73bb83c0831340b8b8f508be9416068476fc0a2
Matt Gross <mattkgross@gmail.com>
2016-06-08 22:53:40 -0600

    Maybe I should use the factory...

--- Extensions/ModelDbContextFactory.cs
+++ Extensions/ModelDbContextFactory.cs
@@ -17,11 +17,19 @@ namespace WorkoutLogPro.Extensions
             { typeof(Team), typeof(TeamContext)}
         };
 
-        public static Type FactoryGetUpdateableDbContext(Type modelType)
+        public static UpdateableDbContext FactoryGetUpdateableDbContext(Type modelType)
         {
             if(map.ContainsKey(modelType))
             {
-                return map[modelType];
+                if (map[modelType].IsSubclassOf(typeof(UpdateableDbContext)))
+                {
+                    return (UpdateableDbContext)Activator.CreateInstance(map[modelType]);
+                }
+                else
+                {
+                    throw new Exception(string.Format("The specified value ({0}) for your key ({1}) was not of the UpdateableDbContext base class.",
+                        map[modelType].ToString(), modelType.ToString()));
+                }
             }
             throw new Exception(string.Format("Cannot find the specified UpdateableDbContext type ({0}).", modelType.ToString()));
         }
